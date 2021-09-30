ed62e15b1529417aa17da5ea66258992893ac3b9
Muhammed <mxgardeee@gmail.com>
2021-09-30 10:53:44 +0200

    Exceptions

---
+++ HerbertShildt/src/main/java/Chapter11/P2/EmployeeImpl.java
@@ -0,0 +1,60 @@
+package Practice11.P2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeImpl {
+
+    
+    private static Employee[] employeeArray = new Employee[10];
+
+    
+    EmployeeImpl() {
+    }
+
+    
+    public void add(Employee emp) throws InvalidOperationException {
+        if (employeeArray[emp.getId()] != null) {
+            throw new InvalidOperationException("Error adding employee , employee id already exists " + emp.getId());
+        }
+        try {
+            employeeArray[emp.getId()] = emp;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new InvalidOperationException("Error adding employee , id must be less than " + employeeArray.length);
+        }
+    }
+
+    
+    public void delete(int id) throws InvalidOperationException {
+        if (employeeArray[id] == null) {
+            throw new InvalidOperationException("Error deleting employee, no such employee " + id);
+        }
+        try {
+            employeeArray[id] = null;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new InvalidOperationException("Error deleting employee, id must be less than " + employeeArray.length);
+        }
+    }
+
+    
+    public Employee findById(int id) throws InvalidOperationException {
+        try {
+            return employeeArray[id];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new InvalidOperationException("Error finding employee ", e);
+        }
+    }
+
+
+    public Employee[] getAllEmployees() {
+        List<Employee> emps = new ArrayList<>();
+        
+        for (Employee e : employeeArray) {
+            if (e != null) {
+                emps.add(e);
+            }
+        }
+        return emps.toArray(new Employee[0]);
+    }
+
+}
---
+++ HerbertShildt/src/main/java/Chapter11/P2/InvalidOperationException.java
@@ -0,0 +1,20 @@
+package Practice11.P2;
+
+public class InvalidOperationException extends Exception {
+
+    public InvalidOperationException() {
+        super();
+    }
+
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+
+    public InvalidOperationException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
